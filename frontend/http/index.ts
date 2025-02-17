7659c97d6cb9b2ba43cf7d73be076cbdd2b14e5d
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-02-18 02:39:40 +0500

    asdfasdf

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = useRuntimeConfig().public.API_URL;
 const $axios = axios.create({
   withCredentials: true,
   baseURL: `${API_URL}/api`,
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: false },
+  devtools: {enabled: false},
   modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt"],
   plugins: ["~/plugins/vue-query.ts"],
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL
+    }
+  }
 });
